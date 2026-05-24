366bfd6005e7d71cd6ce7b67b628490e29e7284f
LucasLixo <paulolucasalves2006@gmail.com>
2026-05-24 00:06:25 -0300

    Remove xdg-mime auto-detection from EmbedFile.cmake, require FILE_MIME from caller

--- cmake/EmbedFile.cmake
+++ cmake/EmbedFile.cmake
@@ -1,22 +1,7 @@
-# Set minimum CMake version
 cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
 
-# Read hex content from generated file
+# Required inputs (passed via -D): FILE_HEX, FILE_KEY, FILE_MIME,
+# EMBED_FILE_PATH, CMAKE_CURRENT_INCLUDE_DIR
 file(READ ${FILE_HEX} CONTENT_HEX)
-
-# Generate SHA256 hash of file key for unique identifiers
 string(SHA256 FILE_KEY_HASH ${FILE_KEY})
-
-# Detect MIME type of the file (optional)
-find_program(XDG_MIME xdg-mime)
-if (XDG_MIME)
-    # Query MIME type using xdg-mime command
-    execute_process(COMMAND ${XDG_MIME} query filetype ${FILE_PATH} OUTPUT_VARIABLE FILE_MIME)
-    # Strip trailing whitespace (newline) from output
-    string(STRIP "${FILE_MIME}" FILE_MIME)
-else()
-    set(FILE_MIME "unsupported")
-endif()
-
-# Generate C source file from template, substituting hex content
-configure_file("${CMAKE_CURRENT_INCLUDE_DIR}/xxd.in" ${EMBED_FILE_PATH})
+configure_file("${CMAKE_CURRENT_INCLUDE_DIR}/xxd.c.in" ${EMBED_FILE_PATH})
