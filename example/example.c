6e5522a357b4501d91983d3c46344efc34e97cff
LucasLixo <paulolucasalves2006@gmail.com>
2026-05-24 00:07:11 -0300

    Refactor example.c to enhance output formatting and include MIME type and size information

--- example/example.c
+++ example/example.c
@@ -1,11 +1,20 @@
 #include "xxd.h"
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
 
 int32_t main(int32_t argc, char* argv[])
 {
-  printf("text.txt\n%s\n", xxd_get("text", NULL, NULL));
-  printf("rect.svg\n%s\n", xxd_get("rect", NULL, NULL));
+  size_t textSize = 0ULL;
+  const char* textMime = NULL;
+  const char* textContent = xxd_get("text", &textSize, &textMime);
+
+  size_t rectSize = 0ULL;
+  const char* rectMime = NULL;
+  const char* rectContent = xxd_get("rect", &rectSize, &rectMime);
+
+  printf("text.txt\n\tMime: %s\n\tSize: %zu\n\tContent: %s\n", textMime, textSize, textContent);
+  printf("rect.svg\n\tMime: %s\n\tSize: %zu\n\tContent: %s\n", rectMime, rectSize, rectContent);
   return (int32_t)0;
 }
